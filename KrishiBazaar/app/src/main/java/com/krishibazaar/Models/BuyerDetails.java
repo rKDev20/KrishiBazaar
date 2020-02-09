c0c4ec4efe737227823c3f19823a064e1327e617
rKDev20 <52132131+rKDev20@users.noreply.github.com>
2020-02-09 18:59:37 +0530

    light update

--- KrishiBazaar/app/src/main/java/com/krishibazaar/Models/BuyerDetails.java
+++ KrishiBazaar/app/src/main/java/com/krishibazaar/Models/BuyerDetails.java
@@ -5,7 +5,6 @@ import org.json.JSONObject;
 
 import static com.krishibazaar.Utils.Constants.DISTANCE;
 import static com.krishibazaar.Utils.Constants.NAME;
-import static com.krishibazaar.Utils.Constants.PINCODE;
 import static com.krishibazaar.Utils.Constants.PRICE;
 import static com.krishibazaar.Utils.Constants.STATUS;
 import static com.krishibazaar.Utils.Constants.TIMESTAMP;
@@ -14,7 +13,7 @@ import static com.krishibazaar.Utils.Constants.TRANSACTION_ID;
 public class BuyerDetails {
     private int tranId;
     private String name;
-    private float price;
+    private Float price;//optional
     private float distance;
     private long timestamp;
     private int status;
@@ -22,9 +21,10 @@ public class BuyerDetails {
     BuyerDetails(JSONObject object) throws JSONException {
         tranId = object.getInt(TRANSACTION_ID);
         name = object.getString(NAME);
-        price = (float) object.getDouble(PRICE);
+        if (object.has(PRICE))
+            price = (float) object.getDouble(PRICE);
         timestamp = object.getLong(TIMESTAMP);
-        distance = object.getInt(DISTANCE);
+        distance = (float) object.getDouble(DISTANCE);
         status = object.getInt(STATUS);
     }
 
