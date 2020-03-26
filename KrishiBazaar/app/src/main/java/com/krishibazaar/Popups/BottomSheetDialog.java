d58c6c19c3825c9ff41372e85b62ec4c03f990e7
lordayush <ayush.ayushk.kumar@gmail.com>
2020-03-26 23:07:24 +0530

    Lockdown Changes 1.0

---
+++ KrishiBazaar/app/src/main/java/com/krishibazaar/Popups/BottomSheetDialog.java
@@ -0,0 +1,20 @@
+package com.krishibazaar.Popups;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.krishibazaar.R;
+
+public class BottomSheetDialog extends BottomSheetDialogFragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.bottom_sheet,container,false);
+    }
+}
